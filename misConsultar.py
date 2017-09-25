c7a5b14ed89758348813344c832fecd816f568ff
fatichikis <fatima@fatima-PC>
2017-09-24 18:57:17 -0700

    Examen Completo

--- misConsultar.py
+++
@@ -1,7 +0,0 @@
-/*Consultas*/
-
-Blog.objects.create(name='Beatles Blog', tagline='All the latest Beatles news.')
-from blog.models import Blog
-b = Blog(name='Beatles Blog', tagline='All the latest Beatles news.')
-b.save()
-
